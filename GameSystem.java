4b4aaf0beadad79990a9f1a745c1f06ba2aec2a7
c.n.speckhardt <@wwu.edu>
2018-05-04 19:17:49 -0700

    added GameSystem, Role, SceneCard

---
+++ GameSystem.java
@@ -0,0 +1,54 @@
+// C.Speckhardt // speckhc // W01240690 // CSCI 345 // 2018.05.04 // 
+
+
+// Game System Class
+
+public class GameSystem{
+      
+      // attributes
+      private int daysRemaining;
+      private Player[] players;
+      public Player currentPlay;
+      public Player nextPlay;
+      
+      // constructor 
+      public GameSystem(int numPlayers){
+            
+            initialize();     
+            
+      }
+  //••••••••••••••••••••••••••••••••••••• INITIALIZE ••••••••••••••••••••••••••••••••••••     
+     
+      public void initialize(){
+      
+            
+      }
+      
+ //••••••••••••••••••••••••••••••••••••• DISPLAY ••••••••••••••••••••••••••••••••••••     
+
+      public void display(){
+      
+      }
+      
+  //••••••••••••••••••••••••••••••••••••• DAY ••••••••••••••••••••••••••••••••••••     
+     
+      private void day(){
+      
+      
+      }
+      
+  //••••••••••••••••••••••••••••••••••••• ROLL ••••••••••••••••••••••••••••••••••••     
+      
+      public int roll(){
+      
+            return 0;
+      }
+      
+  //••••••••••••••••••••••••••••••••••••• ENDGAME ••••••••••••••••••••••••••••••••••••     
+      
+      private void endGame(){
+      
+      
+      }
+
+}
