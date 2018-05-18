0b04ff13224a6f187d04d754f2e94a614e2bcd5c
pickern <nickmpickering@gmail.com>
2018-05-17 23:40:38 -0700

    commit before merge

--- SceneCard.java
+++ SceneCard.java
@@ -1,35 +1,35 @@
-// C.Speckhardt // speckhc // W01240690 // CSCI 345 // 2018.05.04 // 
+// C.Speckhardt // speckhc // W01240690 // CSCI 345 // 2018.05.04 //
 
 
 // SceneCard Class
 
 public class SceneCard{
-      
+
       // attributes
       public String name;
       public int number;
       public String description;
       public int budget;
       public Role[] roles;
-      
-      // constructor 
+
+      // constructor
       public SceneCard(String name, int num, String description, int budget, Role[] roles){
-            
+
             this.name= name;
             this.number= num;
             this.description= description;
             this.budget= budget;
-            this.roles= roles;      
-            
+            this.roles= roles;
+
       }
-      
+
       @Override
       public String toString(){
-            
-            String ans=" "; 
-            
+
+            String ans=" ";
+
            // = this.name+ " ("+ this.description+ ", $"+ this.budget+ " million) \n";
-            
+
             return ans;
       }
 
